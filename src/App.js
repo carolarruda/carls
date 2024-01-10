69df4fa2979a64ec753c45f4cd96a4a0e84eede8
carolina arruda <carolinacalouroarruda@gmail.com>
2024-01-10 17:01:26 +0100

    testing the bug fix for the deployment - fetching initial data

--- src/App.js
+++ src/App.js
@@ -36,32 +36,41 @@ function App() {
           Authorization: `Bearer ${token}`,
         },
       };
+  
+      // Fetch personal recipes
       fetch(`https://node-mysql-api-0zxf.onrender.com/recipes/personal`, opts)
         .then((res) => res.json())
         .then((data) => {
           setRecipesP(data.data.recipes);
         })
-        .catch((error) =>
-          console.error("Error fetching personal recipes:", error)
-        );
+        .catch((error) => {
+          console.error("Error fetching personal recipes:", error);
+        });
+  
+      // Fetch user data
       fetch(`https://node-mysql-api-0zxf.onrender.com/users/${userId}`, opts)
         .then((res) => res.json())
         .then((data) => {
           setUser(data.data.user);
           console.log('myuser', data.data.user);
         })
-        .catch((error) => console.error("Error fetching user:", error));
+        .catch((error) => {
+          console.error("Error fetching user:", error);
+        });
     }
+  
     console.log('fetching public recipes, this might take a while');
     setLoggedIn(false);
-
+  
+    // Fetch public recipes
     fetch(`https://node-mysql-api-0zxf.onrender.com/recipes`)
       .then((res) => res.json())
       .then((data) => {
         setRecipes(data.data);
       })
-      .catch((error) => console.error("Error fetching recipes:", error));
-
+      .catch((error) => {
+        console.error("Error fetching recipes:", error);
+      });
   }, [token, userId]);
 
 
