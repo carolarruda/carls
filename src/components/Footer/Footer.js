c3b1cc7e109f9e19fc1d60f16cc0a0b281906f81
carolina arruda <carolinacalouroarruda@gmail.com>
2024-01-18 15:47:53 +0100

    finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,86 +1,73 @@
-import * as React from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import { Facebook, Instagram, Twitter } from "@mui/icons-material";
-import { Box } from "@mui/material";
-import "../style.css";
-import pic1 from "../images/a028b336ab264b7983e1c895ebb3e8bd-removebg-preview.png";
+import classes from "./Footer.module.css";
+import Logo from "../Logo/Logo";
+import Tiktok from "../icons/socialMedia/Tiktok";
+import X from "../icons/socialMedia/X";
+import Facebook from "../icons/socialMedia/Facebook";
+import Instagram from "../icons/socialMedia/Instagram";
+import Pinterest from "../icons/socialMedia/Pinterest";
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <Box
-      className="footer"
-      component="footer"
-      sx={{
-        marginTop: "auto",
-        display: "grid",
-        placeItems: "end",
-        backgroundColor: (theme) =>
-          theme.palette.mode === "light"
-            ? theme.palette.grey[200]
-            : theme.palette.grey[800],
-        p: 6,
-      }}
-    >
-      <Container maxWidth="lg">
-        <Grid container spacing={6}>
-          <Grid item xs={12} sm={4}>
-            <Typography variant="h6" color="text.primary" gutterBottom>
-              About Us
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              We are Carl's - the food plaza, dedicated to providing the best
-              recipes to our users.
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={3.5}>
-            <Typography variant="h6" color="text.primary" gutterBottom>
-              Contact Us
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              123 Main Street, Anytown, USA
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Email: info@example.com
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Phone: +1 234 567 8901
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={2.5}>
-            <Typography variant="h6" color="text.primary" gutterBottom>
-              Follow Us
-            </Typography>
-            <Link href="https://www.facebook.com/" color="inherit">
-              <Facebook />
-            </Link>
-            <Link
-              href="https://www.instagram.com/"
-              color="inherit"
-              sx={{ pl: 1, pr: 1 }}
-            >
-              <Instagram />
-            </Link>
-            <Link href="https://www.twitter.com/" color="inherit">
-              <Twitter />
-            </Link>
-          </Grid>
-          <Grid item xs={8} sm={1.4}>
-            <img src={pic1} alt="logo" id="img-logo-footer" />
-          </Grid>
-        </Grid>
-        <Box mt={5}>
-          <Typography variant="body2" color="text.secondary" align="center">
-            {"Copyright © "}
-            <Link color="inherit" href="https://carls.vercel.app/">
-              Carl's
-            </Link>{" "}
-            {new Date().getFullYear()}
-          </Typography>
-        </Box>
-      </Container>
-    </Box>
+    <section className={` ${classes.footerContainer}`}>
+      <div className={classes.footerGrid}>
+        <div className={classes.about}>
+          <Logo />
+          <p>
+            A website dedicated to providing the best recipes and your space to
+            showcase your culinary creativity.{" "}
+          </p>
+        </div>
+        <div className={classes.linksContainer}>
+          <ul>
+            <li>Home</li>
+            <li>Recipes</li>
+            <li>Blog</li>
+          </ul>
+          <ul>
+            <li>Share Recipe</li>
+            <li>About Us</li>
+            <li>Contact</li>
+          </ul>
+          <ul>
+            <li>Terms of Use</li>
+            <li>Privacy & Cookies</li>
+          </ul>
+        </div>
+
+        <div className={classes.subscribe}>
+          <h5>Newsletter</h5>
+          <p>Subscribe to our newsletter to get more </p>
+          <input
+            className={classes.input}
+            type="text"
+            placeholder="&#9993; Enter your Email"
+          />
+          <button className={classes.subscribeButton}>Subscribe</button>
+        </div>
+      </div>
+
+      <div className={classes.footerBottom}>
+        <p>© 2023 PerfectRecipe All Right Reserved</p>
+        <div className={classes.logos}>
+          <div className={classes.logo}>
+            <Tiktok />
+          </div>
+          <div className={classes.logo}>
+            <X />
+          </div>
+          <div className={classes.logo}>
+            <Facebook />
+          </div>
+          <div className={classes.logo}>
+            <Instagram />
+          </div>
+          <div >
+            <Pinterest />{" "}
+          </div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Footer;
