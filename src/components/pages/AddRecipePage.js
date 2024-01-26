cb4f1d95627e2ef1b1f9aee999a3b6ff8548285a
carolina arruda <carolinacalouroarruda@gmail.com>
2024-01-26 17:06:07 +0100

    minor fixes

--- src/components/pages/AddRecipePage.js
+++ src/components/pages/AddRecipePage.js
@@ -1,16 +1,12 @@
-import RecipeAdd from "../RecipeAddUpdate/RecipeAdd";
 import Footer from "../Footer/Footer";
 import classes from "./pages.module.css";
-import Header from "../Header/Header";
 import NewRecipe from "../NewRecipe/NewRecipe";
 
 const AddRecipePage = ({ setRecipes, setRecipesP, update }) => {
   return (
     <main className={classes}>
       <div className={classes.section}>
-
         <NewRecipe />
-
       </div>
       <Footer />
     </main>
