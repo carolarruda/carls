f74c037f1ce1cae9b44204c125be6b33d238f962
carolina arruda <carolinacalouroarruda@gmail.com>
2024-02-04 11:49:50 +0100

    finished lateral newsletter

--- src/components/Newsletter/NewsletterSmall.js
+++ src/components/Newsletter/NewsletterSmall.js
@@ -2,17 +2,20 @@ import classes from "./Newsletter.module.css";
 
 const NewsletterSmall = () => {
   return (
-    <section className={`${classes.newsletterContainer}`}>
-      <h3 className={classes.titleSmall}>
+    <section className={`${classes.newsletterContainerLateral}`}>
+      <h3 className={classes.titleSmallLateral}>
         Stay connected with our Recipes updates
       </h3>
-      <p className={classes.newsletter}>
+      <p className={classes.newsletterLaterall}>
         for the latest health tips and delicious recipes
       </p>
-      <div className={classes.inputContainer}>
-        <input placeholder="Enter your Email" type="text" />
-        <button className={classes.newsletterButton}>Subscribe </button>
-      </div>
+
+      <input
+        placeholder="      Enter your Email"
+        type="text"
+        className={classes.inputNewsletter}
+      />
+      <button className={classes.newsletterButtonLateral}>Sign up </button>
     </section>
   );
 };
